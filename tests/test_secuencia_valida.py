817c8c0a8d43126d6458a27f33957a2dbdb54b1e
Luciano Belardo <belardoluciano@gmail.com>
2021-06-14 21:34:02 -0300

    Verifico los test: 98%

--- tests/test_secuencia_valida.py
+++ tests/test_secuencia_valida.py
@@ -1,7 +1,9 @@
 from src.four_line import secuenciaValida
 
 def test_secuencia_valida():
-  secuenciaF = [1, 5, 6, 8, 2, 4, 0]
-  assert False == secuenciaValida(secuenciaF)
   secuenciaT = [1, 5, 6, 4, 2, 4, 7]
   assert True == secuenciaValida(secuenciaT)
+
+def test_secuencia_INvalida():
+  secuenciaF = [1, 5, 6, 8, 2, 4, 0]
+  assert False == secuenciaValida(secuenciaF)
